66f7c580a7309507420defc5ab566e513882f424
DT5Gen <marinovod@gmail.com>
2022-06-08 02:12:22 +0300

    Добавил кнопку и ресайклер

--- app/src/main/java/com/dt5gen/gitapp/MainActivity.kt
+++ app/src/main/java/com/dt5gen/gitapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.dt5gen.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.dt5gen.gitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.refreshButton.setOnClickListener {
+            Toast.makeText(this, "Работает!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
