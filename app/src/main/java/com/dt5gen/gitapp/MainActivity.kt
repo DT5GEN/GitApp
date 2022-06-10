d7714f4ba8303dffe8c18066a7a3620732da7131
DT5Gen <marinovod@gmail.com>
2022-06-10 20:09:09 +0300

    added a progress bar

--- app/src/main/java/com/dt5gen/gitapp/MainActivity.kt
+++ app/src/main/java/com/dt5gen/gitapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dt5gen.gitapp
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dt5gen.gitapp.databinding.ActivityMainBinding
 
@@ -16,13 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        showProgress(false)
 
         binding.refreshButton.setOnClickListener {
             Toast.makeText(this, "Работает!", Toast.LENGTH_SHORT).show()
+            showProgress(true)
 
             usersRepo.getUsers(
-                onSuccess = adapter::setData,
+                onSuccess = {
+                    showProgress(false)
+                    adapter.setData(it) },
                 onError = {
+                    showProgress(false)
                     Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
                 }
             )
@@ -36,4 +42,10 @@ class MainActivity : AppCompatActivity() {
         binding.usersRecyclerView.adapter = adapter
 
     }
+
+    private fun showProgress(inProgress: Boolean) {
+        binding.progressBar.isVisible = inProgress
+        binding.usersRecyclerView.isVisible = !inProgress
+    }
+
 }
